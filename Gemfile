f1ca64712863a633e07ed37c0caecaba3b189703
Daniel Westendorf <daniel@prowestech.com>
2018-03-30 10:34:35 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
+
+gem 'breezy_pdf', '0.0.14'
+gem 'sinatra'
---
+++ app.rb
@@ -0,0 +1,50 @@
+require 'sinatra'
+require 'breezy_pdf'
+
+# Configure Breezy PDF
+BreezyPDF.setup do |config|
+  # Obtain your very own API key from https://breezypdf.com
+  config.secret_api_key             = ENV['BREEZY_PDF_SECRET_API_KEY']
+
+  # See the breezy_pdf gem's documentation for configuration information
+  # https://github.com/danielwestendorf/breezy_pdf-ruby#configuration
+  config.middleware_path_matchers   = [/locations\.pdf/]
+  config.treat_urls_as_private      = true
+  config.upload_assets              = true
+  config.extract_metadata           = true
+  config.threads                    = Concurrent.processor_count
+  config.filter_elements            = true
+  config.filter_elements_selectors  = %w[.breezy-pdf-remove]
+  config.logger.level               = Logger::INFO
+end
+
+use BreezyPDF::Middleware
+set :public_folder, File.dirname(__FILE__) + '/public'
+enable :sessions
+
+get '/' do
+  redirect "/locations"
+end
+
+get '/locations' do
+  session[:locations] ||= [
+    { latitude: "40.7127753", longitude: "-74.0059728", location_name: "New York" },
+    { latitude: "34.0522342", longitude: "-118.2436849", location_name: "Los Angeles" },
+  ]
+  erb :index
+end
+
+post '/locations' do
+  session[:locations].push({
+    latitude: params["latitude"], longitude: params["longitude"], location_name: params["location_name"]
+  })
+
+  redirect "/"
+end
+
+delete '/locations/:index' do
+  i = params[:index].to_i
+  session[:locations].delete_at(i) if session[:locations][i]
+
+  redirect "/"
+end
